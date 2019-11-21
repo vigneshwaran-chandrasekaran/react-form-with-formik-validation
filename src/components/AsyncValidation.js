c6f7ffe45326ad835cc3fdbd7376c591bde1ea79
vigneshwaran-chandrasekaran <ac.vigneshwaran@gmail.com>
2019-11-21 20:36:07 +0530

    AsyncValidation.js file form render method changed

--- src/components/AsyncValidation.js
+++ src/components/AsyncValidation.js
@@ -1,22 +1,22 @@
-import React from 'react';
-import { Formik, Field, Form, ErrorMessage } from 'formik';
-import { Debug } from './Debug';
-import Line from './Line';
+import React from "react";
+import { Formik, Field, Form, ErrorMessage } from "formik";
+import { Debug } from "./Debug";
+import Line from "./Line";
 
 // Async Validation
 const sleep = ms => new Promise(resolve => setTimeout(resolve, ms));
 
-const validate = (values) => {
-  return sleep(300).then((res) => {
+const validate = values => {
+  return sleep(300).then(res => {
     console.log(res);
     let errors = {};
 
-    if (['admin', 'null', 'god'].includes(values.username)) {
-      errors.username = 'Nice try';
+    if (["admin", "null", "god"].includes(values.username)) {
+      errors.username = "Nice try";
     }
 
     if (!values.username) {
-      errors.username = 'Required';
+      errors.username = "Required";
     }
 
     if (Object.keys(errors).length) {
@@ -26,17 +26,17 @@ const validate = (values) => {
 };
 
 const handleOnSubmit = (values, { setSubmitting, resetForm }) => {
-  console.log('handleOnSubmit');
+  console.log("handleOnSubmit");
   console.log({ values });
   sleep(500).then(() => {
     alert(JSON.stringify(values, null, 2));
     resetForm();
     setSubmitting(false);
   });
-}
+};
 
 const initialValues = {
-  username: '',
+  username: ""
 };
 
 const Username = () => (
@@ -46,24 +46,35 @@ const Username = () => (
       initialValues={initialValues}
       validate={validate}
       onSubmit={handleOnSubmit}
-      render={({ isSubmitting, handleReset, errors, touched }) => (
+    >
+      {({ isSubmitting, handleReset, errors, touched }) => (
         <Form>
           <label htmlFor="username">Username</label>
           <Field className="form-control" name="username" type="text" />
-          <ErrorMessage className='text-danger small' component="div" name="username" />
+          <ErrorMessage
+            className="text-danger small"
+            component="div"
+            name="username"
+          />
           <button
             className="btn btn-primary m-2"
             type="submit"
-            disabled={isSubmitting}>Submit</button>
+            disabled={isSubmitting}
+          >
+            Submit
+          </button>
           <button
             type="reset"
             className="btn btn-warning m-2"
             disabled={isSubmitting}
-            onClick={handleReset}>Reset</button>
+            onClick={handleReset}
+          >
+            Reset
+          </button>
           <Debug />
         </Form>
       )}
-    />
+    </Formik>
     <Line />
   </>
 );
