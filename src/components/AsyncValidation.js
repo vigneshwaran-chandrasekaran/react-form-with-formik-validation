c96e3072ddc8c2b73720547ebdeff403000907d8
vigneshwaran-chandrasekaran <ac.vigneshwaran@gmail.com>
2020-02-06 23:21:15 +0530

    added code formating to 3 files

--- src/components/AsyncValidation.js
+++ src/components/AsyncValidation.js
@@ -1,82 +1,86 @@
-import React from "react";
-import { Formik, Field, Form, ErrorMessage } from "formik";
-import { Debug } from "./Debug";
-import Line from "./Line";
+import React from 'react';
+import { Formik, Field, Form, ErrorMessage } from 'formik';
+import { Debug } from './Debug';
+import Line from './Line';
 
 // Async Validation
 const sleep = ms => new Promise(resolve => setTimeout(resolve, ms));
 
 const validate = values => {
-  return sleep(300).then(res => {
-    console.log(res);
-    let errors = {};
+	return sleep(300).then(res => {
+		console.log(res);
+		let errors = {};
 
-    if (["admin", "null", "god"].includes(values.username)) {
-      errors.username = "Nice try";
-    }
+		if (['admin', 'null', 'god'].includes(values.username)) {
+			errors.username = 'Nice try';
+		}
 
-    if (!values.username) {
-      errors.username = "Required";
-    }
+		if (!values.username) {
+			errors.username = 'Required';
+		}
 
-    if (Object.keys(errors).length) {
-      throw errors;
-    }
-  });
+		if (Object.keys(errors).length) {
+			throw errors;
+		}
+	});
 };
 
 const handleOnSubmit = (values, { setSubmitting, resetForm }) => {
-  console.log("handleOnSubmit");
-  console.log({ values });
-  sleep(500).then(() => {
-    alert(JSON.stringify(values, null, 2));
-    resetForm();
-    setSubmitting(false);
-  });
+	console.log('handleOnSubmit');
+	console.log({ values });
+	sleep(500).then(() => {
+		alert(JSON.stringify(values, null, 2));
+		resetForm();
+		setSubmitting(false);
+	});
 };
 
 const initialValues = {
-  username: ""
+	username: '',
 };
 
 const Username = () => (
-  <>
-    <h3>AsyncValidation Pick a username</h3>
-    <Formik
-      initialValues={initialValues}
-      validate={validate}
-      onSubmit={handleOnSubmit}
-    >
-      {({ isSubmitting, handleReset, errors, touched }) => (
-        <Form>
-          <label htmlFor="username">Username</label>
-          <Field className="form-control" name="username" type="text" />
-          <ErrorMessage
-            className="text-danger small"
-            component="div"
-            name="username"
-          />
-          <button
-            className="btn btn-primary m-2"
-            type="submit"
-            disabled={isSubmitting}
-          >
-            Submit
-          </button>
-          <button
-            type="reset"
-            className="btn btn-warning m-2"
-            disabled={isSubmitting}
-            onClick={handleReset}
-          >
-            Reset
-          </button>
-          <Debug />
-        </Form>
-      )}
-    </Formik>
-    <Line />
-  </>
+	<>
+		<h3>AsyncValidation Pick a username</h3>
+		<Formik
+			initialValues={initialValues}
+			validate={validate}
+			onSubmit={handleOnSubmit}
+		>
+			{({ isSubmitting, handleReset, errors, touched }) => (
+				<Form>
+					<label htmlFor="username">Username</label>
+					<Field
+						className="form-control"
+						name="username"
+						type="text"
+					/>
+					<ErrorMessage
+						className="text-danger small"
+						component="div"
+						name="username"
+					/>
+					<button
+						className="btn btn-primary m-2"
+						type="submit"
+						disabled={isSubmitting}
+					>
+						Submit
+					</button>
+					<button
+						type="reset"
+						className="btn btn-warning m-2"
+						disabled={isSubmitting}
+						onClick={handleReset}
+					>
+						Reset
+					</button>
+					<Debug />
+				</Form>
+			)}
+		</Formik>
+		<Line />
+	</>
 );
 
 export default Username;
--- src/components/CombinedValidations.js
+++ src/components/CombinedValidations.js
@@ -1,105 +1,94 @@
-import React from "react";
-import { Formik, Field, Form, ErrorMessage } from "formik";
-import * as Yup from "yup";
-import { Debug } from "./Debug";
-import Line from "./Line";
+import React from 'react';
+import { Formik, Field, Form, ErrorMessage } from 'formik';
+import * as Yup from 'yup';
+import { Debug } from './Debug';
+import Line from './Line';
 
 const Schema = Yup.object().shape({
-  email: Yup.string().required("This field is required")
+	email2: Yup.string().required('This field is required'),
 });
 
 // Async Validation
 const sleep = ms => new Promise(resolve => setTimeout(resolve, ms));
 
 const validate = values => console.log({ values });
-sleep(300).then(() => {
-  let errorMessage = { zip: "This field is required" };
-  throw errorMessage;
-});
 
 const isRequired = message => value => (!!value ? undefined : message);
 
 const FieldLevelValidation = () => (
-  <>
-    <h3>CombinedValidations Pick a username</h3>
-    <Formik
-      validationSchema={Schema}
-      validate={validate}
-      initialValues={{
-        username: "",
-        email: "",
-        zip: ""
-      }}
-      onSubmit={values => {
-        sleep(500).then(() => {
-          alert(JSON.stringify(values, null, 2));
-        });
-      }}
-    >
-      {({
-        errors,
-        touched,
-        setFieldValue,
-        setFieldTouched,
-        validateField,
-        validateForm
-      }) => (
-        <Form>
-          <label htmlFor="username">Username</label>
-          <div>
-            <Field
-              className="form-control"
-              name="username"
-              validate={isRequired("This field is required")}
-              type="text"
-              placeholder="username"
-            />
-            <ErrorMessage
-              className="text-danger small"
-              component="div"
-              name="username"
-            />
-          </div>
-          <br />
-          <div>
-            <Field
-              className="form-control"
-              name="email2"
-              validate={isRequired("This field is required")}
-              type="text"
-              placeholder="email"
-            />
-            <ErrorMessage
-              className="text-danger small"
-              component="div"
-              name="email"
-            />
-          </div>
-          <br />
-          <div>
-            <Field
-              className="form-control"
-              name="zip"
-              validate={isRequired("This field is required")}
-              type="text"
-              placeholder="zip"
-            />
-            <ErrorMessage
-              className="text-danger small"
-              component="div"
-              name="zip"
-            />
-          </div>
-          <br />
-          <button className="btn btn-primary m-2" type="submit">
-            Submit
-          </button>
-          <Debug />
-        </Form>
-      )}
-    </Formik>
-    <Line />
-  </>
+	<>
+		<h3>CombinedValidations Pick a username</h3>
+		<Formik
+			validationSchema={Schema}
+			validate={validate}
+			initialValues={{
+				username: '',
+				email2: '',
+				zip: '',
+			}}
+			onSubmit={values => {
+				sleep(500).then(() => {
+					alert(JSON.stringify(values, null, 2));
+				});
+			}}
+		>
+			{() => (
+				<Form>
+					<label htmlFor="username">Username</label>
+					<div>
+						<Field
+							className="form-control"
+							name="username"
+							validate={isRequired('This field is required')}
+							type="text"
+							placeholder="username"
+						/>
+						<ErrorMessage
+							className="text-danger small"
+							component="div"
+							name="username"
+						/>
+					</div>
+					<br />
+					<div>
+						<Field
+							className="form-control"
+							name="email2"
+							validate={isRequired('This field is required')}
+							type="text"
+							placeholder="email"
+						/>
+						<ErrorMessage
+							className="text-danger small"
+							component="div"
+							name="email2"
+						/>
+					</div>
+					<br />
+					<div>
+						<Field
+							className="form-control"
+							name="zip"
+							validate={isRequired('This field is required')}
+							type="text"
+							placeholder="zip"
+						/>
+						<ErrorMessage
+							className="text-danger small"
+							component="div"
+							name="zip"
+						/>
+					</div>
+					<br />
+					<button className="btn btn-primary m-2" type="submit">
+						Submit
+					</button>
+					<Debug />
+				</Form>
+			)}
+		</Formik>
+		<Line />
+	</>
 );
 
 export default FieldLevelValidation;
