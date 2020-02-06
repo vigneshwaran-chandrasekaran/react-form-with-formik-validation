c91f617e8b7f7537fb897e65cc509fb6861ad5b9
vigneshwaran-chandrasekaran <ac.vigneshwaran@gmail.com>
2020-02-06 22:56:39 +0530

    added prettierrc for formating

--- src/components/AsyncSubmit.js
+++ src/components/AsyncSubmit.js
@@ -5,39 +5,60 @@ import Line from './Line';
 
 const sleep = ms => new Promise(resolve => setTimeout(resolve, ms));
 
+/**
+ * will wait some time to submit the form using sleep
+ */
+
 const AsyncSubmit = () => (
-  <>
-    <h3>AsyncSubmit Sign Up</h3>
-    <Formik
-      initialValues={{
-        firstName: '',
-        lastName: '',
-        email: '',
-      }}
-      onSubmit={async values => {
-        await sleep(1000);
-        alert(JSON.stringify(values, null, 2));
-      }}
-    >
-      {({ isSubmitting }) => (
-        <Form>
-          <label htmlFor="firstName">First Name</label>
-          <Field className="form-control" name="firstName" placeholder="Jane" />
+	<>
+		<h3>AsyncSubmit Sign Up</h3>
+		<Formik
+			initialValues={{
+				firstName: '',
+				lastName: '',
+				email: '',
+			}}
+			onSubmit={async values => {
+				await sleep(1000);
+				alert(JSON.stringify(values, null, 2));
+			}}
+		>
+			{({ isSubmitting }) => (
+				<Form>
+					<label htmlFor="firstName">First Name</label>
+					<Field
+						className="form-control"
+						name="firstName"
+						placeholder="Jane"
+					/>
 
-          <label htmlFor="lastName">Last Name</label>
-          <Field className="form-control" name="lastName" placeholder="Doe" />
+					<label htmlFor="lastName">Last Name</label>
+					<Field
+						className="form-control"
+						name="lastName"
+						placeholder="Doe"
+					/>
 
-          <label htmlFor="email">Email</label>
-          <Field className="form-control" name="email" placeholder="jane@acme.com" type="email" />
-          <button className="btn btn-primary mt-2" type="submit" disabled={isSubmitting}>
-            Submit
-                </button>
-          <Debug />
-        </Form>
-      )}
-    </Formik>
-    <Line />
-  </>
+					<label htmlFor="email">Email</label>
+					<Field
+						className="form-control"
+						name="email"
+						placeholder="jane@acme.com"
+						type="email"
+					/>
+					<button
+						className="btn btn-primary mt-2"
+						type="submit"
+						disabled={isSubmitting}
+					>
+						Submit
+					</button>
+					<Debug />
+				</Form>
+			)}
+		</Formik>
+		<Line />
+	</>
 );
 
 export default AsyncSubmit;
