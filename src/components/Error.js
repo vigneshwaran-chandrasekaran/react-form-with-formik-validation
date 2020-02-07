536881d41deaf48c718b5418bc72e11a0d4db62a
vigneshwaran-chandrasekaran <ac.vigneshwaran@gmail.com>
2020-02-07 19:43:40 +0530

    Debug and Error component formated

--- src/components/Error.js
+++ src/components/Error.js
@@ -1,11 +1,25 @@
 import React from 'react';
 
 export default function Error({ touched, message }) {
-    if (!touched) {
-        return <div>&nbsp;</div>;
-    }
-    if (message) {
-        return <div className='text-danger'>{message} <span role="img" aria-label='Emoji'>😟</span></div>;
-    }
-    return <div className='text-success'>all good <span role="img" aria-label='Emoji'>😃</span></div>;
+	if (!touched) {
+		return <div>&nbsp;</div>;
+	}
+	if (message) {
+		return (
+			<div className="text-danger">
+				{message}{' '}
+				<span role="img" aria-label="Emoji">
+					😟
+				</span>
+			</div>
+		);
+	}
+	return (
+		<div className="text-success">
+			all good{' '}
+			<span role="img" aria-label="Emoji">
+				😃
+			</span>
+		</div>
+	);
 }
