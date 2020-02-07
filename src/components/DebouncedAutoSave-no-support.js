780365c8646757db617b73cbc660d8222ad591b1
vigneshwaran-chandrasekaran <ac.vigneshwaran@gmail.com>
2020-02-07 19:43:01 +0530

    debounceAutoSave page formated

--- src/components/DebouncedAutoSave-no-support.js
+++ src/components/DebouncedAutoSave-no-support.js
@@ -1,77 +1,86 @@
-import React from "react";
-import { useFormikContext, Formik, Field, Form } from "formik";
-import { Debug } from "./Debug";
-import debounce from "just-debounce-it";
+import React from 'react';
+import { useFormikContext, Formik, Field, Form } from 'formik';
+import { Debug } from './Debug';
+import debounce from 'just-debounce-it';
 
 const AutoSave = ({ debounceMs }) => {
-  const formik = useFormikContext();
-  const [lastSaved, setLastSaved] = React.useState(null);
-  const debouncedSubmit = React.useCallback(
-    debounce(
-      () =>
-        formik.submitForm().then(() => setLastSaved(new Date().toISOString())),
-      debounceMs
-    ),
-    [debounceMs, formik.submitForm]
-  );
+	const formik = useFormikContext();
+	const [lastSaved, setLastSaved] = React.useState(null);
+	const debouncedSubmit = React.useCallback(
+		debounce(
+			() =>
+				formik
+					.submitForm()
+					.then(() => setLastSaved(new Date().toISOString())),
+			debounceMs
+		),
+		[debounceMs, formik.submitForm]
+	);
 
-  React.useEffect(() => {
-    debouncedSubmit();
-  }, [debouncedSubmit, formik.values]);
+	React.useEffect(() => {
+		debouncedSubmit();
+	}, [debouncedSubmit, formik.values]);
 
-  return (
-    <>
-      {!!formik.isSubmitting
-        ? "saving..."
-        : lastSaved !== null
-        ? `Last Saved: ${lastSaved}`
-        : null}
-    </>
-  );
+	return (
+		<>
+			{!!formik.isSubmitting
+				? 'saving...'
+				: lastSaved !== null
+				? `Last Saved: ${lastSaved}`
+				: null}
+		</>
+	);
 };
 
 const AutoSavingForm = () => (
-  <div>
-    <Formik
-      initialValues={{
-        firstName: "",
-        lastName: "",
-        email: ""
-      }}
-      onSubmit={(values, { setSubmitting }) => {
-        return new Promise(resolve =>
-          setTimeout(() => {
-            console.log("submitted", JSON.stringify(values, null, 2));
-            setSubmitting(false);
-            resolve();
-          }, 1000)
-        );
-      }}
-    >
-      {() => (
-        <Form>
-          <h3>
-            AutoSavingForm{" "}
-            <small style={{ color: "gray", fontSize: 11 }}>
-              <AutoSave debounceMs={300} />
-            </small>
-          </h3>
+	<div>
+		<Formik
+			initialValues={{
+				firstName: '',
+				lastName: '',
+				email: '',
+			}}
+			onSubmit={(values, { setSubmitting }) => {
+				return new Promise(resolve =>
+					setTimeout(() => {
+						console.log(
+							'submitted',
+							JSON.stringify(values, null, 2)
+						);
+						setSubmitting(false);
+						resolve();
+					}, 1000)
+				);
+			}}
+		>
+			{() => (
+				<Form>
+					<h3>
+						AutoSavingForm{' '}
+						<small style={{ color: 'gray', fontSize: 11 }}>
+							<AutoSave debounceMs={300} />
+						</small>
+					</h3>
 
-          <label htmlFor="firstName">First Name</label>
-          <Field name="firstName" placeholder="Jane" />
+					<label htmlFor="firstName">First Name</label>
+					<Field name="firstName" placeholder="Jane" />
 
-          <label htmlFor="lastName">Last Name</label>
-          <Field name="lastName" placeholder="Doe" />
+					<label htmlFor="lastName">Last Name</label>
+					<Field name="lastName" placeholder="Doe" />
 
-          <label htmlFor="email">Email</label>
-          <Field name="email" placeholder="jane@acme.com" type="email" />
-          <button type="submit">Submit</button>
+					<label htmlFor="email">Email</label>
+					<Field
+						name="email"
+						placeholder="jane@acme.com"
+						type="email"
+					/>
+					<button type="submit">Submit</button>
 
-          <Debug />
-        </Form>
-      )}
-    </Formik>
-  </div>
+					<Debug />
+				</Form>
+			)}
+		</Formik>
+	</div>
 );
 
 export default AutoSavingForm;
