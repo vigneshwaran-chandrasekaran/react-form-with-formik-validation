e5c59a19b563557f35f1588a645ea5db3aa4cbd0
vigneshwaran-chandrasekaran <ac.vigneshwaran@gmail.com>
2020-02-16 13:43:38 +0530

    added more describe suits to add more test coverage

--- src/__tests__/CombinedValidations.test.js
+++ src/__tests__/CombinedValidations.test.js
@@ -7,23 +7,28 @@ const person = {
 	zipcode: faker.address.zipCode(),
 };
 
-describe('h3 Text', () => {
+let browser;
+let page;
+
+beforeAll(async () => {
+	browser = await puppeteer.launch({
+		headless: false,
+		// devtools: true,
+		// slowMo: 80,
+	});
+	page = await browser.newPage();
+
+	page.emulate({
+		viewport: {
+			width: 500,
+			height: 2400,
+		},
+		userAgent: '',
+	});
+});
+
+describe('Check page load', () => {
 	test('h3 loads correctly', async () => {
-		let browser = await puppeteer.launch({
-			headless: false,
-			// devtools: true,
-			// slowMo: 250,
-		});
-		let page = await browser.newPage();
-
-		page.emulate({
-			viewport: {
-				width: 500,
-				height: 2400,
-			},
-			userAgent: '',
-		});
-
 		/**
 		 * we can use data-testid for element selection
 		 * [data-testid="CombinedValidationsBtn"]
@@ -32,42 +37,53 @@ describe('h3 Text', () => {
 
 		await page.goto('http://localhost:3000/');
 		await page.waitForSelector('.CombinedValidations');
-
 		const html = await page.$eval('.CombinedValidations', e => e.innerHTML);
 		expect(html).toBe('CombinedValidations Pick a username2');
+	}, 9000000);
+});
 
+describe('Check empty fileds check', () => {
+	test('Click submit button click', async () => {
 		await page.focus('input[name=username2]');
-
 		await page.click('[data-testid="CombinedValidationsBtn"]');
+	}, 9000000);
 
+	test('Check empty username2 fileds check', async () => {
 		const username2Error = await page.$eval(
 			'[data-testid="username2Error"]',
 			e => e.innerHTML
 		);
 		expect(username2Error).toBe('username2 is required');
+	}, 9000000);
 
+	test('Check empty email2Error fileds check', async () => {
 		const email2Error = await page.$eval(
 			'[data-testid="email2Error"]',
 			e => e.innerHTML
 		);
 		expect(email2Error).toBe('email2 is required');
+	}, 9000000);
 
+	test('Check empty zip2 fileds check', async () => {
 		const zip2Error = await page.$eval(
 			'[data-testid="zip2Error"]',
 			e => e.innerHTML
 		);
 		expect(zip2Error).toBe('zip2 is required');
+	}, 9000000);
+});
 
+describe('Check fiels with value', () => {
+	test('Click submit button with values', async () => {
 		await page.click('input[name=username2]');
 		await page.type('input[name=username2]', person.name);
 		await page.click('input[name=email2]');
 		await page.type('input[name=email2]', person.email);
-
 		await page.click('input[name=zip2]');
 		await page.type('input[name=zip2]', person.zipcode);
-
 		await page.click('[data-testid="CombinedValidationsBtn"]');
-
-		browser.close();
 	}, 9000000);
+});
+afterAll(() => {
+	browser.close();
 });
